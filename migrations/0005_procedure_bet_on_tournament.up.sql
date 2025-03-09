1372a87678e8a1f50ada952086b4c9096e6ece58
DusanDjordjic <dukidjordjic@gmail.com>
2025-03-09 10:04:56 +0100

    update bet on tournament proc to return last row

--- migrations/0005_procedure_bet_on_tournament.up.sql
+++ migrations/0005_procedure_bet_on_tournament.up.sql
@@ -1,6 +1,7 @@
 CREATE PROCEDURE BetOnTournament(IN player_id BIGINT, IN tournament_id BIGINT, IN amount DOUBLE)
 BEGIN
 	DECLARE player_account_balance DOUBLE DEFAULT 0;
+	DECLARE last_id BIGINT DEFAULT 0;
 
 	DECLARE EXIT HANDLER FOR SQLSTATE VALUE '45001'
 	BEGIN
@@ -31,6 +32,12 @@ BEGIN
 	UPDATE players SET account_balance = account_balance - amount WHERE id = player_id;
 
 	INSERT INTO tournament_bets (player_id, tournament_id, bet_amount) VALUES (player_id, tournament_id, amount);
+
+	SET last_id = LAST_INSERT_ID();
+
+	SELECT id, created_at, player_id, tournament_id, bet_amount
+	FROM tournament_bets
+	WHERE id = last_id;
 
 	COMMIT;
 END;
