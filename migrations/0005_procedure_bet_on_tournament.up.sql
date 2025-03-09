30f679a33549c886167874ca7803e1ca507dfb39
DusanDjordjic <dukidjordjic@gmail.com>
2025-03-09 09:48:18 +0100

    add bet on tournament procedure

---
+++ migrations/0005_procedure_bet_on_tournament.up.sql
@@ -0,0 +1,36 @@
+CREATE PROCEDURE BetOnTournament(IN player_id BIGINT, IN tournament_id BIGINT, IN amount DOUBLE)
+BEGIN
+	DECLARE player_account_balance DOUBLE DEFAULT 0;
+
+	DECLARE EXIT HANDLER FOR SQLSTATE VALUE '45001'
+	BEGIN
+		ROLLBACK;
+		RESIGNAL SET MYSQL_ERRNO = 10000, MESSAGE_TEXT = "insufficient funds";
+	END;
+
+	DECLARE EXIT HANDLER FOR SQLEXCEPTION
+	BEGIN
+		ROLLBACK;
+		RESIGNAL;
+	END;
+
+	START TRANSACTION;
+
+	SELECT account_balance INTO player_account_balance
+	FROM players
+	WHERE id = player_id FOR UPDATE;
+
+	IF player_account_balance < amount THEN
+		ROLLBACK;
+		SIGNAL SQLSTATE '45001';
+	ELSEIF player_account_balance = 0 THEN
+		ROLLBACK;
+		SIGNAL SQLSTATE '45001';
+	END IF;
+
+	UPDATE players SET account_balance = account_balance - amount WHERE id = player_id;
+
+	INSERT INTO tournament_bets (player_id, tournament_id, bet_amount) VALUES (player_id, tournament_id, amount);
+
+	COMMIT;
+END;
